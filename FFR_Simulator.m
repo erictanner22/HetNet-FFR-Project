81610e9663d83c586619b29988841b1941f73b09
erictanner22 <erictanner22@gmail.com>
2020-04-05 18:34:13 -0400

    Update FFR_Simulator.m
    Adding changes made from today's zoom working meeting.

--- FFR_Simulator.m
+++ FFR_Simulator.m
@@ -21,6 +21,7 @@ BER     = 10^-6;         % Target Bit Error Rate(BER)
 alpha   = -1.5/log(BER); % Constant for target BER
 deltaf  = 15e3;          % Subcarrier spacing (Hz)
 CH_BW   = 20e6;          % Channel Bandwidth (Hz)
+Mc_rad  = 500;           % Macrocell radius(m)
 m_users = 150;           % Macrocell Users
 f_users = 150;           % Femtocell Users
 SCpRB   = 12;            % Sub-carriers per resource block
@@ -64,32 +65,56 @@ PL = PL_vec(PL_type);
 %-------------------------------------------
 % Macrocell - SINR, Capacity, and Throughput
 %-------------------------------------------
-idx = 0;
-Nm_vec = 5:5:200;
-for Num_Mc = Nm_vec
+idx=0;
+Tm_vec = [];
+Nf_vec = 30:210;
+for Nf=Nf_vec
     
-    Ch_Gain = 10^-(PL/10);
+    Num_Mc = 7;
+    Num_Fc = Nf;
     
+    % Convert Macrocell power to watts
     MC_TxP = MC_TxP(1);
     MC_TxP_W = 10^(MC_TxP/10);
     
+    % Convert Femtocell power to watts
     FC_TxP_W = 10^(FC_TxP/10);
+    
+    
+    % d_vec = [
+    %     866  %B->A
+    %     866  %B->C
+    %     1500 %B->D
+    %     1732 %B->E
+    %     1500 %B->F
+    %     866  %B->G
+    %     ];
     
     % Summation of M neighboring Macro-cell's Power & Gain products on sub-carrier k
     sigma_PMp_GMp = 0; % Initialize to zero
     for m=1:(Num_Mc-1)
-        sigma_PMp_GMp = sigma_PMp_GMp + (MC_TxP_W*Ch_Gain);
+        %d = d_vec(m);
+        d=866;
+        PL_outdoor = 28.0 + 35*log10(d);
+        PL = PL_outdoor;
+        Gain = 10^-(PL/10);
+        sigma_PMp_GMp = sigma_PMp_GMp + (MC_TxP_W*Gain);
     end
     
     % Summation of F neighboring Femto-cell Power & Gain products on sub-carrier k
     sigma_PF_GF = 0; % Initialize to zero
-    %for f=1:(Nf-1)
-    for f=1:(Num_Mc)
-        sigma_PF_GF = sigma_PF_GF + (FC_TxP_W*Ch_Gain);
+    for f=1:(Num_Fc)
+        d = round(rand*(2*Mc_rad));
+        PL_outdoor = 28.0 + 35*log10(d);
+        PL = PL_outdoor;
+        Gain = 10^-(PL/10);
+        sigma_PF_GF = sigma_PF_GF + (FC_TxP_W*Gain);
     end
     
+    
+    Ch_Gain_W = 10^(28/10);
     % SINR equation for a given Macro-cell on sub-carrier k
-    SINRmk = (MC_TxP_W*Ch_Gain)/(10^((No_PSD*deltaf)/10) + sigma_PMp_GMp + sigma_PF_GF);
+    SINRmk = (MC_TxP_W*Ch_Gain_W)/(10^((No_PSD*deltaf)/10) + sigma_PMp_GMp + sigma_PF_GF);
     
     % Capacity of macro user m on sub-carrier k
     Cmk = deltaf*log2(1+alpha*SINRmk);
@@ -97,14 +122,15 @@ for Num_Mc = Nm_vec
     % Calculate Throughput of the Macro-cell across all m users
     Tm = 0; % Initialize to zero
     for m=1:m_users
-        Tm = Tm + Cmk;
+        Beta_km = round(rand);
+        Tm = Tm + Cmk * Beta_km;
     end
     
-    % Store the throughput into the vector (vector used for plotting)
     idx = idx+1;
     Tm_vec(idx) = Tm;
     
 end
+    
 
 %-------------------------------------------
 % Femtocell - SINR, Capacity, and Throughput
@@ -175,6 +201,6 @@ end
 %--------------------------------------------------------------------------
 
 figure; 
-plot(Nm_vec,Tm_vec, 'o');
+plot(Nf_vec,Tm_vec, 'o');
 xlabel('Number of Macro-cells');
 ylabel('Throughput (bps)');
